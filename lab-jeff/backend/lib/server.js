49503da8315c95ba297f974238690feadadf8ce7
jjkusowski <jjkusowski@yahoo.com>
2018-01-22 15:48:09 -0800

    id added to schools

--- lab-jeff/backend/lib/server.js
+++ lab-jeff/backend/lib/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./logger');
 
@@ -10,51 +11,51 @@ let httpServer = null;
 
 mongoose.Promise = Promise;
 
+app.use(cors());
+
 app.use(require('../route/school-router'));
 app.use(require('../route/student-router'));
 
-app.all('*', (request,response) => {
-  logger.log('info','Returning a 404 from the catch-all route');
+app.all('*', (request, response) => {
+  logger.log('info', 'Returning a 404 from the catch-all route');
   return response.sendStatus(404);
 });
 
 app.use(require('./error-middleware'));
 
-const server = module.exports = {};
+const server = (module.exports = {});
 
 server.start = () => {
-  return new Promise((resolve,reject) => {
-    if(isServerOn){
-      logger.log('error','__SERVER_ERROR__ server is already on');
+  return new Promise((resolve, reject) => {
+    if (isServerOn) {
+      logger.log('error', '__SERVER_ERROR__ server is already on');
       return reject(new Error('__SERVER_ERROR__ server is already on'));
     }
     httpServer = app.listen(process.env.PORT, () => {
       isServerOn = true;
       console.log(`Server is listening on port ${process.env.PORT}`);
-      logger.log('info',`Server is listening on port ${process.env.PORT}`);
+      logger.log('info', `Server is listening on port ${process.env.PORT}`);
       return resolve();
     });
-  })
-    .then( () => mongoose.connect(process.env.MONGODB_URI, {useMongoClient : true}));
+  }).then(() => mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true }));
 };
 
 server.stop = () => {
-  return new Promise((resolve,reject) => {
-    if(!isServerOn){
-      logger.log('error','__SERVER_ERROR__ server is already off');
+  return new Promise((resolve, reject) => {
+    if (!isServerOn) {
+      logger.log('error', '__SERVER_ERROR__ server is already off');
       return reject(new Error('__SERVER_ERROR__ server is already off'));
     }
 
-    if(!httpServer){
-      logger.log('error','__SERVER_ERROR__ there is no server to close');
+    if (!httpServer) {
+      logger.log('error', '__SERVER_ERROR__ there is no server to close');
       return reject(new Error('__SERVER_ERROR__ there is no server to close'));
     }
     httpServer.close(() => {
       isServerOn = false;
       httpServer = null;
-      logger.log('info','Server off');
+      logger.log('info', 'Server off');
       return resolve();
     });
-  })
-    .then( () => mongoose.disconnect() );
+  }).then(() => mongoose.disconnect());
 };
