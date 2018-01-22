7f77531ddb8331b30d0fe24177118d34f486d0f9
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-22 15:12:22 -0800

    reworked schemas to match budget tracker front end

---
+++ lab-cameron/backend/model/category.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  budget: {
+    type: Number,
+    required: true,
+  },
+  uuid: {
+    type: String,
+    required: true,
+  },
+  timeStamp: {
+    type: Date,
+    default: () => new Date(),
+  },
+  expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'expense'}],
+}, {
+  usePushEach: true,
+});
+
+module.exports = mongoose.model('school', categorySchema);
