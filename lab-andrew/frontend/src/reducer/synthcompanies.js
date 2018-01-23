69119a5cd2a370cee3e5f5b99cb872661e4e5d5b
Andrew Bloom <blooma116@gmail.com>
2018-01-22 19:12:12 -0800

    added validation to synthCompany create reducer

--- lab-andrew/frontend/src/reducer/synthcompanies.js
+++ lab-andrew/frontend/src/reducer/synthcompanies.js
@@ -1,17 +1,20 @@
 const emptyState = [];
 
+const validateSynthCompany = synthCompany => {
+  if (!synthCompany.name || !synthCompany.location){
+    throw new Error('synthCompany requires both a name and a location');
+  }
+};
+
 export default (state = emptyState, {type, payload}) => {
   switch (type){
     case 'SYNTHCOMPANY_CREATE':
+      validateSynthCompany(payload);
       return [...state, payload];
     case 'SYNTHCOMPANY_UPDATE':
       return state.map(synthCompany => synthCompany.id === payload.id ? payload : synthCompany);
-    case 'SYNTHCOMPANY_REMOVE': {
-      let newState = state.filter(synthCompany => {
-        return synthCompany.id !== payload.id;
-      });
-      return newState;
-    }
+    case 'SYNTHCOMPANY_REMOVE':
+      return state.filter(synthCompany => synthCompany.id !== payload.id);
     case 'SYNTHCOMPANY_CLEAR':
       return emptyState;
     default:
